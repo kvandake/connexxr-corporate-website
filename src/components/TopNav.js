f1943ce30a249db851e8ccb9e4b59731edea361e
Bazyl <info@azbsupply.com>
2022-02-09 13:04:36 -0500

    first commit1

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 
-const TopNav = ({ colorInvert = false }) => {
+const TopNav = () => {
   return (
     <Box display={'flex'} justifyContent={'flex-end'} alignItems={'center'}>
 
