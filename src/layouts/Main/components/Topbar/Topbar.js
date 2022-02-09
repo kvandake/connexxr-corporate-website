8d21707610dd63ffe743049ca2fa3ad7accc67ac
Bazyl <info@azbsupply.com>
2022-02-09 13:58:06 -0500

    remove burger button

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -60,7 +60,7 @@ const Topbar = ({ onSidebarOpen, pages, colorInvert = false }) => {
           />
         </Box> */}
       </Box>
-      <Box sx={{ display: { xs: 'block', md: 'none' } }} alignItems={'center'}>
+      {/* <Box sx={{ display: { xs: 'block', md: 'none' } }} alignItems={'center'}>
         <Button
           onClick={() => onSidebarOpen()}
           aria-label="Menu"
@@ -74,7 +74,7 @@ const Topbar = ({ onSidebarOpen, pages, colorInvert = false }) => {
         >
           <MenuIcon />
         </Button>
-      </Box>
+      </Box> */}
     </Box>
   );
 };
